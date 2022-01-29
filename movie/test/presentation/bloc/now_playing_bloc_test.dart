0036a5b1f582ddcdc7b364292211f7016b7bad23
karuntos <s32190083@student.ubm.ac.id>
2022-01-29 17:26:32 +0700

    added now playing bloc test file

---
+++ movie/test/presentation/bloc/now_playing_bloc_test.dart
@@ -0,0 +1,47 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:core/common/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:movie/presentation/bloc/now_playing_movies/now_playing_movies_cubit.dart';
+
+import '../../dummy_data/dummy_objects.dart';
+import 'usecasemock.mocks.dart';
+
+void main() {
+  late MockGetNowPlayingMovies mockGetNowPlayingMovies;
+  late NowPlayingMoviesCubit nowPlayingCubit;
+
+  setUp((){
+    mockGetNowPlayingMovies = MockGetNowPlayingMovies();
+    nowPlayingCubit = NowPlayingMoviesCubit(mockGetNowPlayingMovies);
+  });
+
+  blocTest<NowPlayingMoviesCubit, NowPlayingMoviesState>(
+      'Should return list of movies when usecase is successful',
+      setUp: () {
+        when(mockGetNowPlayingMovies.execute())
+            .thenAnswer((_) async => Right(testMovieList));
+      },
+      build: () => nowPlayingCubit,
+      act: (cubit) => cubit(),
+      expect: () => [
+        const NowPlayingMoviesLoading(),
+        NowPlayingMoviesResult(testMovieList),
+      ]
+  );
+
+  blocTest<NowPlayingMoviesCubit, NowPlayingMoviesState>(
+    'should return error when usecase said no',
+    setUp: () {
+      when(mockGetNowPlayingMovies.execute())
+          .thenAnswer((_) async => Left(ServerFailure('')));
+    },
+    build: () => nowPlayingCubit,
+    act: (cubit) => cubit(),
+    expect: () => [
+      const NowPlayingMoviesLoading(),
+      const NowPlayingMoviesError('')
+    ]
+  );
+}

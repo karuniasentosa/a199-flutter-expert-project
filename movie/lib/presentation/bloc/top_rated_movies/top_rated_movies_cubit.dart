4abde5fe99d271d08bf4408d98ead1b0c3146537
karuntos <s32190083@student.ubm.ac.id>
2022-01-29 17:27:23 +0700

    removed unused import

--- movie/lib/presentation/bloc/top_rated_movies/top_rated_movies_cubit.dart
+++ movie/lib/presentation/bloc/top_rated_movies/top_rated_movies_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:http/http.dart';
 import 'package:movie/domain/entities/movie.dart';
 
 import 'package:movie/usecases.dart' show GetTopRatedMovies;

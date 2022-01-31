e52966724bdb06360fa9b5b5ca70ce79ea2bb521
karuntos <s32190083@student.ubm.ac.id>
2022-01-31 13:54:45 +0700

    added more blocs.

---
+++ movie/lib/blocs.dart
@@ -0,0 +1,8 @@
+library movie;
+
+export 'presentation/bloc/movie_detail_page_blocs.dart';
+export 'presentation/bloc/movie_list_page_blocs.dart';
+export 'presentation/bloc/popular_movies_page_bloc.dart';
+export 'presentation/bloc/top_rated_movies_page_bloc.dart';
+
+export 'presentation/bloc/search_movies/search_movies_cubit.dart';

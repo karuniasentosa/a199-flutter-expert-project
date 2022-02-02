a3b0bb6085bcf42d56bd631caf7edf72ee7ec3f7
karuntos <s32190083@student.ubm.ac.id>
2022-02-02 13:30:27 +0700

    added blocs to export

--- tv_series/lib/blocs.dart
+++ tv_series/lib/blocs.dart
@@ -2,3 +2,6 @@ export 'presentation/bloc/now_playing_tv_series_page_bloc.dart';
 export 'presentation/bloc/popular_tv_series_page_bloc.dart';
 export 'presentation/bloc/tv_series_detail_page_bloc.dart';
 export 'presentation/bloc/tv_series_list_page_bloc.dart';
+
+export 'presentation/bloc/search_tv_series/search_tv_series_cubit.dart';
+export 'presentation/bloc/watchlist_tv_series/watchlist_tv_series_cubit.dart';

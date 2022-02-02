31eb9c8cee45da6a984d6a33aca40220fb4fd2bc
karuntos <s32190083@student.ubm.ac.id>
2022-02-02 11:27:45 +0700

    creted blocs file for tv_series_list_page.dart

---
+++ tv_series/lib/presentation/bloc/tv_series_list_page_bloc.dart
@@ -0,0 +1,3 @@
+export 'popular_tv_series/popular_tv_series_cubit.dart';
+export 'tv_series_recommendation/tv_series_recommendation_cubit.dart';
+export 'now_playing_tv_series/now_playing_tv_series_cubit.dart';

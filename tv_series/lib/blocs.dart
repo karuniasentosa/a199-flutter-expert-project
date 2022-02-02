d9b223da8ea3bee68c7589f4954dc8ac4d5fc6d7
karuntos <s32190083@student.ubm.ac.id>
2022-02-02 11:33:26 +0700

    added new blocs

---
+++ tv_series/lib/blocs.dart
@@ -0,0 +1,4 @@
+export 'presentation/bloc/now_playing_tv_series_page_bloc.dart';
+export 'presentation/bloc/popular_tv_series_page_bloc.dart';
+export 'presentation/bloc/tv_series_detail_page_bloc.dart';
+export 'presentation/bloc/tv_series_list_page_bloc.dart';

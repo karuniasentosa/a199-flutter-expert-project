aff84417c2bf69d32df4632acc51191bba7b5a84
karuntos <s32190083@student.ubm.ac.id>
2022-02-02 13:21:09 +0700

    added tv series cubit

---
+++ tv_series/lib/presentation/bloc/search_tv_series/search_tv_series_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:tv_series/domain/entities/tv_series.dart';
+import 'package:tv_series/usecases.dart' show SearchTvSeries;
+
+part 'search_tv_series_state.dart';
+
+class SearchTvSeriesCubit extends Cubit<SearchTvSeriesState> {
+  final SearchTvSeries searchTvSeries;
+  SearchTvSeriesCubit(this.searchTvSeries) : super(SearchTvSeriesInitial());
+
+  Future call(String query) async {
+    emit(const SearchTvSeriesLoading());
+    final result = await searchTvSeries.execute(query);
+    result.fold(
+        (l) => emit(SearchTvSeriesError(l.message)),
+        (r) => emit(SearchTvSeriesResult(r))
+    );
+  }
+}
---
+++ tv_series/lib/presentation/bloc/search_tv_series/search_tv_series_state.dart
@@ -0,0 +1,34 @@
+part of 'search_tv_series_cubit.dart';
+
+abstract class SearchTvSeriesState extends Equatable {
+  const SearchTvSeriesState();
+}
+
+class SearchTvSeriesInitial extends SearchTvSeriesState {
+  @override
+  List<Object> get props => [];
+}
+
+class SearchTvSeriesLoading extends SearchTvSeriesState {
+  const SearchTvSeriesLoading();
+  @override
+  List<Object?> get props => [];
+}
+
+class SearchTvSeriesResult extends SearchTvSeriesState {
+  final List<TvSeries> tvSeries;
+
+  const SearchTvSeriesResult(this.tvSeries);
+
+  @override
+  List<Object?> get props => [tvSeries];
+}
+
+class SearchTvSeriesError extends SearchTvSeriesState {
+  final String errorMessage;
+
+  const SearchTvSeriesError(this.errorMessage);
+
+  @override
+  List<Object?> get props => [errorMessage];
+}

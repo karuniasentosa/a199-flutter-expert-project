4a8d07271349727d6de335e3d6bab13615d78918
karuntos <s32190083@student.ubm.ac.id>
2022-02-02 11:30:27 +0700

    created blocs for tv_series_detail_page.dart

---
+++ tv_series/lib/presentation/bloc/tv_series_detail_page_bloc.dart
@@ -0,0 +1,3 @@
+export 'tv_series_detail/tv_series_detail_cubit.dart';
+export 'watchlist_bloc/tv_series_watchlist_bloc.dart';
+export 'tv_series_recommendation/tv_series_recommendation_cubit.dart';

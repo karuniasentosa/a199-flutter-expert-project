d569fc526b87f5ead84d3396b5729581bf58f62e
karuntos <s32190083@student.ubm.ac.id>
2022-02-02 11:24:21 +0700

    added now playing tv series cubit code

---
+++ tv_series/lib/presentation/bloc/now_playing_tv_series/now_playing_tv_series_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:tv_series/domain/entities/tv_series.dart';
+import 'package:tv_series/usecases.dart' show GetNowPlayingTvSeries;
+
+part 'now_playing_tv_series_state.dart';
+
+class NowPlayingTvSeriesCubit extends Cubit<NowPlayingTvSeriesState> {
+  final GetNowPlayingTvSeries getNowPlayingTvSeries;
+
+  NowPlayingTvSeriesCubit(this.getNowPlayingTvSeries) : super(NowPlayingTvSeriesInitial());
+
+  Future call() async {
+    emit(const NowPlayingTvSeriesLoading());
+    final result = await getNowPlayingTvSeries.execute();
+    result.fold(
+        (l) => emit(NowPlayingTvSeriesError(l.message)),
+        (r) => emit(NowPlayingTvSeriesResult(r))
+    );
+  }
+}

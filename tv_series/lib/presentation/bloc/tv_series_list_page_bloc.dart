ef3d7dfb8662e72c542c0ac1ebc0699c402574e4
karuntos <s32190083@student.ubm.ac.id>
2022-02-02 13:30:00 +0700

    changed exports

--- tv_series/lib/presentation/bloc/tv_series_list_page_bloc.dart
+++ tv_series/lib/presentation/bloc/tv_series_list_page_bloc.dart
@@ -1,3 +1,3 @@
 export 'popular_tv_series/popular_tv_series_cubit.dart';
-export 'tv_series_recommendation/tv_series_recommendation_cubit.dart';
+export 'top_rated_tv_series/top_rated_tv_series_cubit.dart';
 export 'now_playing_tv_series/now_playing_tv_series_cubit.dart';
